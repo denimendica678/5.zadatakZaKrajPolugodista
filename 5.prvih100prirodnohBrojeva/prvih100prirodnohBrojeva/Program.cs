2a9f0d53e5659a1204d34a0a980c0b4eeb0f25d1
denimendica678 <58472463+denimendica678@users.noreply.github.com>
2021-01-14 17:28:49 +0100

    Add files via upload

---
+++ 5.prvih100prirodnohBrojeva/prvih100prirodnohBrojeva/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prvih100prirodnohBrojeva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+
+            for (i = 1; i <= 100; i++){
+
+                Console.WriteLine(i);
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
